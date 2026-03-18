bbfb127606e4f7e92633d3e855528a5b02da0038
RobynAwesome <rkholofelo@gmail.com>
2026-03-18 04:31:39 +0200

    modified:   src/components/Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,17 @@
 const Navbar = () => {
   return (
-    <div className="">
+    <div className="w-full h-16 md:h-20 flex items-center justify-between">
         {/* LOGO */}
-        <div className=""></div>
+        <div className="">
+            <img src="/logo.png" className="w-8 h-8" alt="" />
+            <span>5s-ArenaLogo</span>
+        </div>
         {/* MOBILE MENU */}
+        <div className="md: hidden">M</div>
         {/* DESKTOP MENU */}
+        <div className="hidden md:flex">D</div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
