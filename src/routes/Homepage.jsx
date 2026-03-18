0ece06d18fb7d8bd3b6a41c119e6fe1add63f53a
RobynAwesome <rkholofelo@gmail.com>
2026-03-18 12:21:02 +0200

    Update Homepage.jsx

--- src/routes/Homepage.jsx
+++ src/routes/Homepage.jsx
@@ -1,7 +1,3 @@
 export default function Homepage() {
-  return (
-    <div style={{ padding: "2rem", fontSize: "1.5rem" }}>
-      ✅ Homepage is rendering!
-    </div>
-  );
+  return <div>✅ Homepage is rendering!</div>;
 }
