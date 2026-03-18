98225c43492ec27aa430b2d42c39172bfc5f21d2
RobynAwesome <rkholofelo@gmail.com>
2026-03-18 07:36:28 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,12 @@
+/**
+ * Main App Component
+ * Author: https://www.linkedin.com/in/kholofelo-robyn-rababalela-7a26273b6
+ */
 import Navbar from "./components/Navbar"
 
 const App = () => {
   return (
-    <div className="px-4 md:px-8 lg:px-16 lx:px-32 2xl:px-64">
+    <div className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-64">
       {/* NAVBAR */}
       <Navbar />
       {/* BREADCRUMB */}
