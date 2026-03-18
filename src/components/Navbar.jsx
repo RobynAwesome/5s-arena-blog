32c864c33b2e3a4a50cb7e96317746105a30a7e0
RobynAwesome <rkholofelo@gmail.com>
2026-03-18 04:22:12 +0200

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+const Navbar = () => {
+  return (
+    <div className="">
+        {/* LOGO */}
+        {/* MOBILE MENU */}
+        {/* DESKTOP MENU */}
+    </div>
+  )
+}
+
+export default Navbar
